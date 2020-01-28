144af6f17499da2d268d98f59a82ca0a6387efd1
jahnestacado <ioannis.kiwe@gmail.com>
2020-01-28 12:55:39 +0000

    Adjusted config

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ set +x
 export HOST_IP=`hostname -I | cut -d' ' -f 1`
 
 echo "Copying prometheus config file to /home/jahn/prometheus/"
-cp -f prometheus.yml /home/jahn/shared/prometheus/
+cp -f prometheus.yml /mnt/shared/prometheus/
 
 echo "Setting stack IP to $HOST_IP"
 ls
-docker stack deploy -c docker-stack-armv64v8.yml prometheus-grafana
+docker stack deploy -c docker-stack-armv64v8.yml monitoring
 
